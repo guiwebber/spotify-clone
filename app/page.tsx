6c2a3d10780e320dbe6d7d8e5768053c5b1112df
guilherme <guiz1n.webber@gmail.com>
2025-01-22 19:29:54 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,104 @@
-import Image from "next/image";
-
+import "./globals.css";
+import me from "/image/image.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
+import {
+  faHome,
+  faSearch,
+  faCompass,
+  faDownload,
+  faBell,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-zinc-900 h-screen text-zinc-50 flex flex-col">
+      <header className="py-3 px-5 bg-black flex justify-between align-middle">
+        <div className=" flex">
+          <FontAwesomeIcon icon={faSpotify} width={38} />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        <div className="flex ml-40 w-2/6 items-center ">
+          <button className=" bg-zinc-800 w-12 h-12 rounded-full pl-3 mr-5">
+            <FontAwesomeIcon icon={faHome} width={25} />
+          </button>
+          <div className="bg-zinc-800 rounded-full w-full h-12 flex relative">
+            <button className="p-1 pl-3 absolute top-2.5">
+              <FontAwesomeIcon icon={faSearch} width={20} />
+            </button>
+            <input
+              type="text"
+              placeholder="O que você quer ouvir?"
+              className=" bg-transparent w-full h-12  font-medium pb-1 pl-10"
+            />
+            <button className="pl-2 absolute top-3.5 right-3 border-zinc-500/40 border-l-2">
+              <FontAwesomeIcon icon={faCompass} width={22} />
+            </button>
+          </div>
+        </div>
+
+        <div className="flex h-auto justify-end align-middle items-center   text-zinc-300/90">
+          <div className="flex justify-center align-middle ">
+            <FontAwesomeIcon icon={faDownload} width={20} className="m-1 " />
+            <a href="#" className="m-auto mx-2  font-bold">
+              Instalar aplicativo
+            </a>
+          </div>
+          <button className="mx-10">
+            <FontAwesomeIcon icon={faBell} width={20} className="m-1" />
+          </button>
+          <div className="w-12 h-12 flex justify-center items-center rounded-full bg-zinc-800">
+            <img
+              src="/images/image.png "
+              alt=""
+              className="  w-9 h-9 rounded-full"
+            />
+          </div>
+        </div>
+      </header>
+      <div className=" flex flex-1 ">
+        <aside className="w-80 ">Aside</aside>
+        <main className=" flex-1">Main</main>
+      </div>
+      <footer className="p-2  bg-black flex justify-between">
+        <div className="flex items-center">
+          {" "}
+          <img
+            src="/images/album1.png "
+            alt=""
+            className="  w-14 h-14 rounded-md m-2"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+          <div className=" p-2 ">
+            <a className="font-semibold hover:underline cursor-pointer">
+              Nome da música
+            </a>
+
+            <div>
+              <a
+                href="#"
+                className="mr-2 text-sm text-zinc-400 hover:underline"
+              >
+                Video clipe
+              </a>
+              <a href="" className="text-zinc-400">
+                •{" "}
+              </a>
+              <a
+                href="#"
+                className="mr-2 text-sm text-zinc-400 hover:underline"
+              >
+                Nome da banda
+              </a>
+            </div>
+          </div>
+          <div className="mx-2">
+            <a href="#">
+              <FontAwesomeIcon icon={faPlus} width={15} />
+            </a>
+          </div>
+        </div>
+        <div>tes</div>
+        <div>tes</div>
       </footer>
     </div>
   );
