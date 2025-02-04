ef0f35d06190c6d74b13e7fbc1ce6cfe383f0984
Guilherme webber <guiz1n.webber@gmail.com>
2025-02-04 09:28:36 -0300

    footer and header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,19 @@
-
+'use client'
+// components/Footer.tsx
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay, faPause, faVolumeHigh, faRandom, faChevronLeft, faChevronRight, faRepeat, faMicrophone, faList, faDesktop, faExpand } from "@fortawesome/free-solid-svg-icons";
+import { faPlay, faPause, faVolumeHigh, faVolumeMute, faRandom, faChevronLeft, faChevronRight, faRepeat, faMicrophone, faList, faDesktop, faExpand } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export default function Footer() {
+  const [progress, setProgress] = useState(50);
+  const [volume, setVolume] = useState(70);
+  const [isMuted, setIsMuted] = useState(false);
+
+  const toggleMute = () => {
+    setIsMuted(!isMuted);
+    setVolume(isMuted ? 70 : 0);
+  };
+
   return (
     <footer className="p-2 bg-transparent flex justify-between items-center">
       <div className="flex items-center">
@@ -18,25 +29,47 @@ export default function Footer() {
       </div>
       <div className="flex flex-col items-center text-zinc-400">
         <div className="flex">
-          <button className="m-2 hover:text-zinc-50"><FontAwesomeIcon icon={faRandom} width={25} /></button>
-          <button className="m-2 hover:text-zinc-50"><FontAwesomeIcon icon={faChevronLeft} width={25} /></button>
-          <button className="w-12 h-12 rounded-full m-2 bg-zinc-50"><FontAwesomeIcon icon={faPause} width={25} color="black" /></button>
-          <button className="m-2 hover:text-zinc-50"><FontAwesomeIcon icon={faChevronRight} width={25} /></button>
-          <button className="m-2 hover:text-zinc-50"><FontAwesomeIcon icon={faRepeat} width={25} /></button>
+          <button className="m-2 hover:text-zinc-50"><FontAwesomeIcon icon={faRandom} width={20} className="h-auto" /></button>
+          <button className="m-2 hover:text-zinc-50"><FontAwesomeIcon icon={faChevronLeft} width={20} className="h-auto" /></button>
+          <button className="w-12 h-12 rounded-full m-2 bg-zinc-50"><FontAwesomeIcon icon={faPause} width={20} className="h-auto" color="black" /></button>
+          <button className="m-2 hover:text-zinc-50"><FontAwesomeIcon icon={faChevronRight} width={20} className="h-auto" /></button>
+          <button className="m-2 hover:text-zinc-50"><FontAwesomeIcon icon={faRepeat} width={20} className="h-auto" /></button>
         </div>
-        <div className="flex">
+        <div className="flex items-center w-full">
           <p className="mx-4">00:00</p>
-          <p className="mx-4 hover:text-zinc-50 cursor-pointer">BARRA DE PROGRESSO DA MUSICA</p>
+          <div className="relative w-64 h-1 bg-gray-600 rounded-lg">
+            <div className="absolute top-0 left-0 h-1 bg-zinc-400 rounded-lg" style={{ width: `${progress}%` }}></div>
+            <input
+              type="range"
+              min="0"
+              max="100"
+              value={progress}
+              onChange={(e) => setProgress(Number(e.target.value))}
+              className="absolute w-full h-1 opacity-0 cursor-pointer"
+            />
+          </div>
           <p className="mx-4">10:00</p>
         </div>
       </div>
       <div className="flex items-center text-zinc-400">
-        <button className="mx-2 hover:text-zinc-50"><FontAwesomeIcon icon={faMicrophone} width={20} /></button>
-        <button className="mx-2 hover:text-zinc-50"><FontAwesomeIcon icon={faList} width={25} /></button>
-        <button className="mx-2 hover:text-zinc-50"><FontAwesomeIcon icon={faDesktop} width={25} /></button>
-        <button className="mx-2 hover:text-zinc-50"><FontAwesomeIcon icon={faVolumeHigh} width={25} /></button>
-        <p className="hover:text-zinc-50 cursor-pointer">BARRA DE VOLUME</p>
-        <button className="mx-2 hover:text-zinc-50"><FontAwesomeIcon icon={faExpand} width={24} /></button>
+        <button className="mx-2 hover:text-zinc-50"><FontAwesomeIcon icon={faMicrophone} width={20} className="h-auto" /></button>
+        <button className="mx-2 hover:text-zinc-50"><FontAwesomeIcon icon={faList} width={20} className="h-auto" /></button>
+        <button className="mx-2 hover:text-zinc-50"><FontAwesomeIcon icon={faDesktop} width={20} className="h-auto" /></button>
+        <button className="mx-2 hover:text-zinc-50" onClick={toggleMute}>
+          <FontAwesomeIcon icon={isMuted || volume === 0 ? faVolumeMute : faVolumeHigh} width={20} className="h-auto" />
+        </button>
+        <div className="relative w-24 h-1 bg-gray-600 rounded-lg">
+          <div className="absolute top-0 left-0 h-1 bg-zinc-400 rounded-lg" style={{ width: `${volume}%` }}></div>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={volume}
+            onChange={(e) => setVolume(Number(e.target.value))}
+            className="absolute w-full h-1 opacity-0 cursor-pointer"
+          />
+        </div>
+        <button className="mx-2 hover:text-zinc-50"><FontAwesomeIcon icon={faExpand} width={20} className="h-auto" /></button>
       </div>
     </footer>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,23 +8,23 @@ import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 export default function Header() {
   return (
     <header className="py-3 px-5 bg-transparent flex justify-between items-center">
-      <button><FontAwesomeIcon icon={faSpotify} width={38} /></button>
+      <button><FontAwesomeIcon icon={faSpotify} width={35} className="h-auto" /></button>
       <div className="flex ml-40 w-2/6 items-center relative">
         <button className="absolute left-3">
-          <FontAwesomeIcon icon={faSearch} width={20} />
+          <FontAwesomeIcon icon={faSearch} width={20} className="h-auto" />
         </button>
         <input type="text" placeholder="O que você quer ouvir?" className="bg-zinc-800 w-full h-12 rounded-full font-medium pl-10 text-white" />
         <button className="absolute right-3 border-l-2 border-zinc-500/40 pl-2">
-          <FontAwesomeIcon icon={faCompass} width={22} />
+          <FontAwesomeIcon icon={faCompass} width={20} className="h-auto" />
         </button>
       </div>
       <div className="flex items-center text-zinc-300/90 space-x-6">
         <button className="hover:text-zinc-50 transition-all flex items-center">
-          <FontAwesomeIcon icon={faDownload} width={20} />
+          <FontAwesomeIcon icon={faDownload} width={20} className="h-auto" />
           <span className="ml-2 font-bold">Instalar aplicativo</span>
         </button>
         <button className="hover:text-zinc-50 transition-all">
-          <FontAwesomeIcon icon={faBell} width={20} />
+          <FontAwesomeIcon icon={faBell} width={20} className="h-auto" />
         </button>
         <div className="w-12 h-12 flex justify-center items-center rounded-full bg-zinc-800">
           <img src="/images/image.png" className="w-9 h-9 rounded-full" />
